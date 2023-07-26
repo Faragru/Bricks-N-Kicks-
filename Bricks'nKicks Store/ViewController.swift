cc4e80ab5ec515b9c0a0fdc55eb97770d270647f
Faragru <ciprians39@yahoo.com>
2023-07-26 11:44:53 +0300

    Initial Commit

---
+++ Bricks'nKicks Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bricks'nKicks Store
+//
+//  Created by Stan Ciprian on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
